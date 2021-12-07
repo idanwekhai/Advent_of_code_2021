81cf68b276c29c652dda9b69c7673fa000d7d51a
kelvin paschal <kelvinidanwekhai@gmail.com>
2021-12-07 21:17:57 +0000

    Optimize Day 7

--- Day 7.py
+++ Day 7.py
@@ -21,7 +21,9 @@ def calc_fuel_two(seq):
     for pos in range(0, len(seq)):
         track_fuel = 0
         for i in seq:
-            fuel = sum(range(1, abs(i-pos)+1))
+            #fuel = sum(range(1, abs(i-pos)+1))
+            n = abs(i-pos)+1
+            fuel = int((n*(n+1))/2)
             track_fuel += fuel
         total_fuel[pos] = track_fuel
     return total_fuel
